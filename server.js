1ac88c4e84f51bb6b8236404ccef50fcaedb24ec
Ashwini <ashwiniparaye9309@gmail.com>
2023-04-14 20:21:57 +0530

    style added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const path = require("path");
+
 require("dotenv").config();
 
 const app = express();
@@ -10,8 +12,16 @@ const connectDB = require("./config/db");
 
 connectDB();
 
+//template engine
+
+app.set("views", path.join(__dirname, "/views"));
+
+app.set("view engine", "ejs");
+
 //Routes
 app.use("/api/files", require("./routes/files"));
+
+app.use("/files", require("./routes/show"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
